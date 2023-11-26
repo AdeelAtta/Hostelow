d324c954a02456502ed2242e5b3a54f9a59b1fae
AdeelAtta <adeelatta@gmail.com>
2023-11-26 13:25:28 +0500

    popular destination added

--- src/components/sections/loved-hostels-list.tsx
+++ src/components/sections/loved-hostels-list.tsx
@@ -5,13 +5,13 @@ import { BsFillSuitHeartFill, BsHeart } from "react-icons/bs";
 const lovedHostelsList = () => {
     return (
         <section className="bg-white">
-            <div className="mx-auto max-w-screen-xl px-4 py-12 sm:px-6 lg:px-8 lg:py-16">
+            <div className="mx-auto max-w-screen-2xl px-4 py-12 sm:px-6 lg:px-8 lg:py-16">
                 <h2
                     className="text-left text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl"
                 >
                     Hostels loved by Customers
                 </h2>
-                <div className='flex gap-4 overflow-x-auto mt-10'>
+                <div className=' gap-4 overflow-x-auto mt-10 sm:flex'>
 
 
                     <div className="relative block rounded-lg p-4 shadow-sm shadow-indigo-200 border-2">
@@ -20,7 +20,7 @@ const lovedHostelsList = () => {
                         <img
                             alt="Home"
                             src="https://images.unsplash.com/photo-1613545325278-f24b0cae1224?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-                            className="h-56 w-56 rounded-md object-cover"
+                            className="h-56 w-auto rounded-md object-cover"
                         />
 
                         <div className="mt-2">
@@ -46,7 +46,7 @@ const lovedHostelsList = () => {
                         <img
                             alt="Home"
                             src="https://images.unsplash.com/photo-1613545325278-f24b0cae1224?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-                            className="h-56 w-56 rounded-md object-cover"
+                            className="h-56 w-auto rounded-md object-cover"
                         />
 
                         <div className="mt-2">
@@ -72,7 +72,7 @@ const lovedHostelsList = () => {
                         <img
                             alt="Home"
                             src="https://images.unsplash.com/photo-1613545325278-f24b0cae1224?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-                            className="h-56 w-56 rounded-md object-cover"
+                            className="h-56 w-auto rounded-md object-cover"
                         />
 
                         <div className="mt-2">
