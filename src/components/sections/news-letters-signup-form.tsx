fd56b5a709b0f00201d737baccaa9cbdda9805eb
AdeelAtta <adeelatta@gmail.com>
2024-01-28 17:45:11 +0500

    minor fix

--- src/components/sections/news-letters-signup-form.tsx
+++ src/components/sections/news-letters-signup-form.tsx
@@ -6,7 +6,7 @@ const NewsLettersSignupForm = () => {
             <div className="p-8 md:p-12 lg:px-16 lg:py-24">
                 <div className="mx-auto max-w-lg text-center">
                     <h2 className="text-2xl font-bold text-gray-900 md:text-3xl">
-                        Get Latest Updates from Hostelow
+                        Get Latest Updates from HostelBazaar
                     </h2>
 
                     <p className="hidden text-gray-500 sm:mt-4 sm:block">
