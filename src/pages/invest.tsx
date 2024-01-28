4a2a547924ce3b5e395173a9a18ff32626790ab3
hammadShamir <hammadshamir7@gmail.com>
2024-01-28 15:15:58 +0500

    Fix Code Structure

--- src/pages/invest.tsx
+++ src/pages/invest.tsx
@@ -1,12 +1,114 @@
-import Button from '@/components/elements/Button'
-import InvestForm from '@/components/forms/invest-form'
-import HeroFormSection from '@/components/sections/hero-form-section'
 import Link from 'next/link'
-import React from 'react'
+import Button from '@/components/elements/Button'
+import HeroFormSection from '@/components/sections/hero-form-section'
 
 const invest = () => {
     return (
-        <HeroFormSection />
+        <section className="mx-auto max-w-screen-2xl px-4 py-12 sm:px-6 lg:px-8 lg:py-16">
+            <HeroFormSection />
+            <div className='grid gird-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-8'>
+                <div className="block rounded-lg px-4 py-2 shadow-lg shadow-indigo-100 bg-white ">
+                    <img
+                        alt="Home"
+                        src='/assets/hostel_small.png'
+                        className="h-48 w-full rounded-md object-cover"
+                    />
+                    <h3 className='text-2xl font-bold'>type room</h3>
+                    <div className="mt-2 grid grid-cols-3 py-3 border-l-2 border-r-2">
+                        <div className='flex flex-col items-center justify-center px-2'>
+                            <h4 className='text-[#7D7D7D] text-base font-bold'>availability</h4>
+                            <p className='text-[#7D7D7D] text-sm min-w-fit whitespace-nowrap text-center'>Total Space</p>
+                        </div>
+                        <div className='flex flex-col items-center justify-center px-2 border-l-2 border-r-2 '>
+                            <h4 className='text-[#7D7D7D] text-base font-bold'>occupancy</h4>
+                            <p className='text-[#7D7D7D] text-sm min-w-fit whitespace-nowrap text-center'>Occupied</p>
+                        </div>
+                        <div className='flex flex-col items-center justify-center px-2 '>
+                            <h4 className='text-[#7D7D7D] text-base font-bold'>availability</h4>
+                            <p className='text-[#7D7D7D] text-sm min-w-fit whitespace-nowrap text-center'>Remaining</p>
+                        </div>
+                    </div>
+                    <Link href="">
+                        <Button text='Invest Now!' type='button' customeStyle='w-full my-4' />
+                    </Link>
+                </div>
+                <div className="block rounded-lg px-4 py-2 shadow-lg shadow-indigo-100 bg-white ">
+                    <img
+                        alt="Home"
+                        src='/assets/hostel_small.png'
+                        className="h-48 w-full rounded-md object-cover"
+                    />
+                    <h3 className='text-2xl font-bold'>type room</h3>
+                    <div className="mt-2 grid grid-cols-3 py-3 border-l-2 border-r-2">
+                        <div className='flex flex-col items-center justify-center px-2'>
+                            <h4 className='text-[#7D7D7D] text-base font-bold'>availability</h4>
+                            <p className='text-[#7D7D7D] text-sm min-w-fit whitespace-nowrap text-center'>Total Space</p>
+                        </div>
+                        <div className='flex flex-col items-center justify-center px-2 border-l-2 border-r-2 '>
+                            <h4 className='text-[#7D7D7D] text-base font-bold'>occupancy</h4>
+                            <p className='text-[#7D7D7D] text-sm min-w-fit whitespace-nowrap text-center'>Occupied</p>
+                        </div>
+                        <div className='flex flex-col items-center justify-center px-2 '>
+                            <h4 className='text-[#7D7D7D] text-base font-bold'>availability</h4>
+                            <p className='text-[#7D7D7D] text-sm min-w-fit whitespace-nowrap text-center'>Remaining</p>
+                        </div>
+                    </div>
+                    <Link href="">
+                        <Button text='Invest Now!' type='button' customeStyle='w-full my-4' />
+                    </Link>
+                </div>
+                <div className="block rounded-lg px-4 py-2 shadow-lg shadow-indigo-100 bg-white ">
+                    <img
+                        alt="Home"
+                        src='/assets/hostel_small.png'
+                        className="h-48 w-full rounded-md object-cover"
+                    />
+                    <h3 className='text-2xl font-bold'>type room</h3>
+                    <div className="mt-2 grid grid-cols-3 py-3 border-l-2 border-r-2">
+                        <div className='flex flex-col items-center justify-center px-2'>
+                            <h4 className='text-[#7D7D7D] text-base font-bold'>availability</h4>
+                            <p className='text-[#7D7D7D] text-sm min-w-fit whitespace-nowrap text-center'>Total Space</p>
+                        </div>
+                        <div className='flex flex-col items-center justify-center px-2 border-l-2 border-r-2 '>
+                            <h4 className='text-[#7D7D7D] text-base font-bold'>occupancy</h4>
+                            <p className='text-[#7D7D7D] text-sm min-w-fit whitespace-nowrap text-center'>Occupied</p>
+                        </div>
+                        <div className='flex flex-col items-center justify-center px-2 '>
+                            <h4 className='text-[#7D7D7D] text-base font-bold'>availability</h4>
+                            <p className='text-[#7D7D7D] text-sm min-w-fit whitespace-nowrap text-center'>Remaining</p>
+                        </div>
+                    </div>
+                    <Link href="">
+                        <Button text='Invest Now!' type='button' customeStyle='w-full my-4' />
+                    </Link>
+                </div>
+                <div className="block rounded-lg px-4 py-2 shadow-lg shadow-indigo-100 bg-white ">
+                    <img
+                        alt="Home"
+                        src='/assets/hostel_small.png'
+                        className="h-48 w-full rounded-md object-cover"
+                    />
+                    <h3 className='text-2xl font-bold'>type room</h3>
+                    <div className="mt-2 grid grid-cols-3 py-3 border-l-2 border-r-2">
+                        <div className='flex flex-col items-center justify-center px-2'>
+                            <h4 className='text-[#7D7D7D] text-base font-bold'>availability</h4>
+                            <p className='text-[#7D7D7D] text-sm min-w-fit whitespace-nowrap text-center'>Total Space</p>
+                        </div>
+                        <div className='flex flex-col items-center justify-center px-2 border-l-2 border-r-2 '>
+                            <h4 className='text-[#7D7D7D] text-base font-bold'>occupancy</h4>
+                            <p className='text-[#7D7D7D] text-sm min-w-fit whitespace-nowrap text-center'>Occupied</p>
+                        </div>
+                        <div className='flex flex-col items-center justify-center px-2 '>
+                            <h4 className='text-[#7D7D7D] text-base font-bold'>availability</h4>
+                            <p className='text-[#7D7D7D] text-sm min-w-fit whitespace-nowrap text-center'>Remaining</p>
+                        </div>
+                    </div>
+                    <Link href="">
+                        <Button text='Invest Now!' type='button' customeStyle='w-full my-4' />
+                    </Link>
+                </div>
+            </div>
+        </section>
     )
 }
 
